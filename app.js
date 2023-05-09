266ff1e526ed911b588059bcaad2aaece2974ce1
Amir Alemohammad <amirho3inalemohammad@gmail.com>
2023-05-09 18:45:02 +0430

    create ui swagger for project

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const path = require("path");
 const connectDB = require("./configs/database");
 const {setHeaders} = require("./middlewares/handleHeaders");
 const { errorHandler } = require("./middlewares/errorHandler");
 const errorController = require("./controllers/errorController");
-const {AllRoutes}= require("./routers/router")
+const {AllRoutes}= require("./routers/router");
+const { title } = require("process");
 const app = express();
 
 
@@ -37,9 +40,33 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname , "public")));
 
 
+//Swagger Ui
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerJsDoc({
+    definition : {
+        info : {
+            title : "Amir Store",
+            version : "2.0.0",
+            description : "بزرگترین مرجع آموزش برنامه نویسی و فروش محصولات جذاب برای برنامه نویسان",
+            contact: {
+                name: "Amir-Alemohammad",
+                email: "amirho3inalemohammad@gmail.com",
+            }
+        },
+        servers:[
+            {
+                url : "http://localhost:3000"
+            },
+        ],
+        
+    },
+    apis: ["./routers/*/*.js"],
+
+})));
+
+
 
 //Handle Routes
-app.use(AllRoutes)
+app.use(AllRoutes);
 
 
 //Error Handeling
--- routers/api/index.js
+++ routers/api/index.js
@@ -3,7 +3,27 @@ const {Router} = require("express");
 const homeController = require("../../controllers/api/homeController");
 
 const router = Router();
-
+/**
+ * @swagger
+ * tags:
+ *      name : IndexPage
+ *      description: های صفحه ی اصلی وبسایت api
+ */
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: index of routes
+ *      tags:
+ *          [IndexPage]
+ *      description: get all data for index page
+ *      responses: 
+ *              200:
+ *                  description: success
+ *              404:
+ *                  description: NotFound
+ * 
+ */
 router.get("/",homeController.homePage);
 
 
