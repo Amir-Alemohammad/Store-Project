98be8b4f0b0f66ba71891ddf57e307529a77c922
Amir Alemohammad <amirho3inalemohammad@gmail.com>
2023-07-23 17:33:03 +0430

    create user access level section

--- middlewares/verifyAccessToken.js
+++ middlewares/verifyAccessToken.js
@@ -1,5 +1,8 @@
 const createHttpError = require("http-errors");
 const jwt = require("jsonwebtoken");
+const userModel = require("../models/user");
+
+
 
 const authenticated = async(req,res,next)=>{
 try {
@@ -27,6 +30,35 @@ try {
     next(err);
 }
 }
+
+
+function checkRole(role){
+    return async function (req,res,next){
+        try {
+            const userId = req.userId;
+            const user = await userModel.findById(userId,{
+                password: 0,
+                otp : 0,
+                refreshToken: 0,
+                createdAt: 0,
+                updatedAt: 0,
+                discount: 0,
+                bills: 0,
+                __v: 0,
+            });
+            if(user.Roles.includes(role)) return next()
+            throw createHttpError.Forbidden("شما مجوز ورود ندارید")
+        } catch (error) {
+            next(error)
+        }
+        
+    }
+}
+
+
+
+
 module.exports = {
     authenticated,
+    checkRole,
 }
--- routers/router.js
+++ routers/router.js
@@ -4,6 +4,7 @@ const {HomeRoute} = require("./api/index");
 const {userAuthRoute} = require("./user/authRoute");
 const { categoryRoute } = require("./admin/category.router");
 const { blogRoutes } = require("./admin/blog.router");
+const { authenticated, checkRole } = require("../middlewares/verifyAccessToken");
 
 const router = Router();
 
@@ -12,9 +13,9 @@ router.use("/",HomeRoute);
 
 router.use("/user",userAuthRoute);
 
-router.use("/admin/category",categoryRoute);
+router.use("/admin/category" , authenticated , checkRole("ADMIN") , categoryRoute);
 
-router.use("/admin/blogs",blogRoutes)
+router.use("/admin/blogs" ,authenticated , checkRole("ADMIN") , blogRoutes)
 
 
 
